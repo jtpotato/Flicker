da8525e9cea451113200e72e4c7f8a845527246a
Joel <58995538+jtpotato@users.noreply.github.com>
2023-03-11 16:27:59 +1100

    Make it even more extendable

--- Assets/Flicker/Player/CameraFollow.cs
+++ Assets/Flicker/Player/CameraFollow.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] Camera playerCamera;
     float smoothTime = 0.2f;
     Vector3 velocity = Vector3.zero;
 
     void FixedUpdate()
     {
         Vector3 desiredPos = target.position - new Vector3(0f, 0f, (target.position.y + 10f) * 2f);
-        Vector3 smoothedCamPos = Vector3.SmoothDamp(transform.position, desiredPos, ref velocity, smoothTime);
-        transform.position = smoothedCamPos;
+        Vector3 smoothedCamPos = Vector3.SmoothDamp(playerCamera.transform.position, desiredPos, ref velocity, smoothTime);
+        playerCamera.transform.position = smoothedCamPos;
     }
 }
