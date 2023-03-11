6d492af203aa75c42d0f5082638746851fa1e75d
Joel <58995538+jtpotato@users.noreply.github.com>
2023-03-11 14:22:45 +1100

    Revert "Convert to 3D"
    This reverts commit 467ef260d7432cce86b51ef20e1dfab662a6f170.

--- Assets/Flicker/CameraFollow.cs
+++ Assets/Flicker/CameraFollow.cs
@@ -5,20 +5,13 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] Transform target;
-    float smoothTime = 0.5f;
+    float smoothTime = 0.2f;
     Vector3 velocity = Vector3.zero;
-    [SerializeField] float offsetX;
-    [SerializeField] float offsetY;
-    [SerializeField] float offsetZ;
 
-    void Update()
+    void FixedUpdate()
     {
-        // Get desired direction from camera to target
-        Vector3 desiredDir = target.rotation * new Vector3(offsetX, offsetY, offsetZ);
-        Vector3 desiredPos = target.position + desiredDir;
+        Vector3 desiredPos = target.position - new Vector3(0f, 0f, (target.position.y + 10f) * 2f);
         Vector3 smoothedCamPos = Vector3.SmoothDamp(transform.position, desiredPos, ref velocity, smoothTime);
         transform.position = smoothedCamPos;
-        
-        transform.LookAt(target);
     }
 }
---
+++ Assets/Flicker/Controls/Controls.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Controls : NetworkBehaviour
+{
+    float movementX;
+    float jumpValue;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] float speed;
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+    }
+    void OnJump(InputValue jumpControlValue)
+    {
+        jumpValue = jumpControlValue.Get<float>();
+    }
+    void OnNormalAttack(InputValue normalAttackControlValue)
+    {
+        
+    }
+
+    void Update()
+    {
+        if (!isLocalPlayer) return;
+
+        rb.velocity = new Vector2(Mathf.Ceil(movementX) * speed, rb.velocity.y);
+
+        // Jump
+        if (jumpValue > 0 && rb.position.y <= 20f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpValue * 20f);
+            jumpValue = 0;
+        }
+    }
+}
